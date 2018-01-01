e7ee3348aa53b363b69f4e1de2a752405fcdd021
Florian CONESA <florian.conesa@laposte.net>
2018-01-01 14:39:04 +0100

    Initialisation

---
+++ carte_adhesion.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+Plugin Name: Carte adhérent FFMC
+Plugin URI: https://www.ffmc80.com
+Description: Plugin d'affichage et d'impression d'une carte d'adhérent
+Version: 0.1
+Author: Florian CONESA
+Author URI: https://www.ffmc80.com
+License: GPL2
+*/
+
+?>
